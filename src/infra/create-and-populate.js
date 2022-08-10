e52f9933b2423f52c5b9f85946505c0825a87fba
BrunoNasc74 <bruno.hpn74@gmail.com>
2022-08-10 02:27:36 -0300

    ✨ feat: populando schemas de cardapio e bebidas

--- src/infra/create-and-populate.js
+++ src/infra/create-and-populate.js
@@ -34,43 +34,85 @@ function populaTabelaClientes() {
 	});
 }
 
-// Schema de cardápio
+// Criando e populando schema de cardápio
 
-// const CARDAPIO_SCHEMA = `
-// CREATE TABLE IF NOT EXISTS "CARDAPIO" (
-//     "ID" INTEGER PRIMARY KEY AUTOINCREMENT
-//     "Nome" VARCHAR(32),
-//     "Categoria" VARCHAR(32),
-//     "Descrição" VARCHAR(200),
-//     "Imagem" VARCHAR(100),
-//     "Preço" INT
-// );`;
+const CARDAPIO_SCHEMA = `
+CREATE TABLE IF NOT EXISTS "CARDAPIO" (
+    "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "Nome" VARCHAR(100),
+    "Categoria" VARCHAR(32),
+    "Descricao" VARCHAR(200),
+    "Imagem" VARCHAR(500),
+    "Porcao" VARCHAR(20),
+    "Preco" INT
+)`;
 
-// function criaTabelaCardapio() {
-// 	db.run(CARDAPIO_SCHEMA, error => {
-// 		if (error) console.log('Erro ao criar tabela de cardápio');
-// 	});
-// }
+const ADD_CARDAPIO_DATA = `
+INSERT INTO CARDAPIO (ID, NOME, CATEGORIA, DESCRICAO, IMAGEM, PORCAO, PRECO)
+VALUES
+    (1, 'Pão de queijo',                 'Comida regional', 'Massa de polvilho e queijo, diretamente de Minas',                     
+    'https://cdn.discordapp.com/attachments/988224567619244072/1006791890265448468/pao-de-queijo.jpeg', '6 unidades', '8'),
+    (2, 'Acarajé',                       'Comida regional', 'Bolinho de feijão fradinho, recheado com vatapá, vinagrete e camarão', 
+    'https://cdn.discordapp.com/attachments/988224567619244072/1006791708853415977/acaraje.jpg', '500 gramas', '22'),
+    (3, 'Almôndegas ao molho',           'Comida casual',   'Deliciosas almôndegas com molho à escolha',                            
+    'https://cdn.discordapp.com/attachments/988224567619244072/1006791889242050611/almondega.jpg', '500 gramas', '25'),
+    (4, 'Risotto ao camarão',            'Comida italiana', 'Arroz arbóreo ao molho com camarão',                                   
+    'https://cdn.discordapp.com/attachments/988224567619244072/1006791889892151327/risoto-de-camarao.jpg', '400 gramas', '68'),
+    (5, 'Filet Mignon ao molho madeira', 'Comida de rico',  'Carne nobre com preço exorbitante',                                    
+    'https://cdn.discordapp.com/attachments/988224567619244072/1006791889573396480/file-mignon-ao-molho-madeira.jpg', '250 gramas', '97')`;
 
-// // Schema de bebidas
+function criaTabelaCardapio() {
+	db.run(CARDAPIO_SCHEMA, error => {
+		if (error) console.log('Erro ao criar tabela de cardápio');
+	});
+}
 
-// const BEBIDAS_SCHEMA = `
-// CREATE TABLE IF NOT EXISTS "BEBIDAS" (
-//     "ID" INTEGER PRIMARY KEY AUTOINCREMENT
-//     "Nome" VARCHAR(32),
-//     "Categoria" VARCHAR(32),
-//     "Descrição" VARCHAR(200),
-//     "Imagem" VARCHAR(100),
-//     "Preço" INT
-// );`;
+function populaTabelaCardapio() {
+	db.run(ADD_CARDAPIO_DATA, error => {
+		if (error) console.log('Erro ao popular tabela de cardápio');
+	});
+}
 
-// function criaTabelaCardapio() {
-// 	db.run(CARDAPIO_SCHEMA, error => {
-// 		if (error) console.log('Erro ao criar tabela de cardápio');
-// 	});
-// }
+// Criando e populando schema de bebidas
 
-// // Schema de pedidos
+const BEBIDAS_SCHEMA = `
+CREATE TABLE IF NOT EXISTS "BEBIDAS" (
+    "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "Nome" VARCHAR(32),
+    "Categoria" VARCHAR(32),
+    "Descricao" VARCHAR(200),
+    "Imagem" VARCHAR(100),
+    "Porcao" VARCHAR(20),
+    "Preco" INT
+)`;
+
+const ADD_BEBIDAS_DATA = `
+INSERT INTO BEBIDAS (ID, NOME, CATEGORIA, DESCRICAO, IMAGEM, PORCAO, PRECO)
+VALUES
+    (1, 'Guaraná Mineiro', 'Refrigerante',    'Deliciosa bebida refrescante', 
+    'https://cdn.discordapp.com/attachments/988224567619244072/1006794688520519740/guarana-mineiro.jpeg', '2 litros', '8'),
+    (2, 'Coca-Cola',       'Refrigerante',    'Deliciosa bebida refrescante', 
+    'https://cdn.discordapp.com/attachments/988224567619244072/1006794864547074099/coca-cola.webp', '2 litros', '7'),
+    (3, 'Água de Coco',    'Bebida tropical', 'Água fresca direta do coco',   
+    'https://cdn.discordapp.com/attachments/988224567619244072/1006795031593615490/agua-de-coco.jpg', '500 ml', '7'),
+    (4, 'Suco de laranja', 'Suco',            'Suco natural de laranja',      
+    'https://cdn.discordapp.com/attachments/988224567619244072/1006795177739948082/suco-de-laranja.jpg', '500 ml', '5'),
+    (5, 'Caipirinha',      'Bebida alcólica', 'Proibido para menores',        
+    'https://cdn.discordapp.com/attachments/988224567619244072/1006795627591639050/caipirinha.webp', '500 ml', '20')`;
+
+function criaTabelaBebidas() {
+	db.run(BEBIDAS_SCHEMA, error => {
+		if (error) console.log('Erro ao criar tabela de bebidas');
+	});
+}
+
+function populaTabelaBebidas() {
+	db.run(ADD_BEBIDAS_DATA, error => {
+		if (error) console.log('Erro ao popular tabela de bebidas');
+	});
+}
+
+// // Criando e populando schema de pedidos
 
 // const PEDIDOS_SCHEMA = `
 // CREATE TABLE IF NOT EXISTS "PEDIDOS" (
@@ -86,7 +128,7 @@ function populaTabelaClientes() {
 // 	});
 // }
 
-// // Schema de mesas
+// // Criando e populando schema de mesas
 
 // const MESAS_SCHEMA = `
 // CREATE TABLE IF NOT EXISTS "MESAS" (
@@ -100,57 +142,11 @@ function populaTabelaClientes() {
 // 	});
 // }
 
-// // Populando tabela
-
-// const ADD_USUARIOS_DATA = `
-// INSERT INTO USUARIOS (ID, NOME, EMAIL, SENHA)
-// VALUES
-//     (1, 'Eugênio Oliveira', 'eugenio.oliveira@bol.com.br', '*******'),
-//     (2, 'Olívia Ribeiro', 'olivia.ribeiro@gmail.com', '********'),
-//     (3, 'Mirtes Faria Lima', 'mirtes_fl@yahoo.com', '********')
-// `;
-
-// function populaTabelaUsr() {
-// 	db.run(ADD_USUARIOS_DATA, error => {
-// 		if (error) console.log('Erro ao popular tabela de usuários');
-// 	});
-// }
-
-// //==== Tarefas
-// const TAREFAS_SCHEMA = `
-// CREATE TABLE IF NOT EXISTS TAREFAS (
-//     ID INTEGER PRIMARY KEY AUTOINCREMENT,
-//     TITULO VARCHAR(64),
-//     DESCRICAO TEXT,
-//     STATUS VARCHAR(32),
-//     DATACRIACAO VARCHAR(32),
-//     ID_USUARIO INTEGER,
-//     FOREIGN KEY(ID_USUARIO) REFERENCES USUARIOD(ID)
-// );`;
-
-// const ADD_TAREFAS_DATA = `INSERT INTO TAREFAS (TITULO, DESCRICAO, STATUS, DATACRIACAO, ID_USUARIO)
-// VALUES
-//        ('Yoga', 'Fazer yoga segunda e quarta', 'Continuo', '2021-01-10', 2),
-//        ('Médico', 'Consulta com Dr. Ayrton sexta', 'TODO', '2021-01-13', 1),
-//        ('Pagar contas', 'Pagar boletos de água e luz', 'DOING', '2021-01-02', 2),
-//        ('Mercado', 'Pegar lista na geladeira e fazer compras', 'TODO', '2021-01-08', 2),
-//        ('Dentista', 'Consulta com Dra Andreia sexta', 'TODO', '2021-01-11', 1),
-//        ('Pagar financiamento carro', 'Pagar parcela do mês do financiamento', 'Contínuo', '2021-01-05', 3)
-// `;
-
-// function criaTabelaTarefas() {
-// 	db.run(TAREFAS_SCHEMA, error => {
-// 		if (error) console.log('Erro ao criar tabela de Tarefas');
-// 	});
-// }
-
-// function populaTabelaTarefas() {
-// 	db.run(ADD_TAREFAS_DATA, error => {
-// 		if (error) console.log('Erro ao popular tabela de Tarefas');
-// 	});
-// }
-
 db.serialize(() => {
 	criaTabelaClientes();
 	populaTabelaClientes();
+	criaTabelaCardapio();
+	populaTabelaCardapio();
+	criaTabelaBebidas();
+	populaTabelaBebidas();
 });
