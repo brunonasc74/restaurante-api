a27493601f341e54152749e86b8453791a4541e8
brunonasc74 <bruno.hpn74@gmail.com>
2022-08-14 15:35:58 -0300

    refatorado index do router

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 const clientes = require('./clientesRoute');
 const cardapios = require('./cardapiosRoute');
+const bebidas = require('./bebidasRoute');
 const pedidos = require('./pedidosRoute');
 const mesas = require('./mesasRoute');
-const bebidas = require('./bebidasRoute');
 
 module.exports = (app) => {
 	app.use(clientes);
 	app.use(cardapios);
+	app.use(bebidas);
 	app.use(pedidos);
 	app.use(mesas);
-	app.use(bebidas);
 };
