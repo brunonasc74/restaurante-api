4987ad04562f685bff471f40cf13e114ba4d9a7b
brunonasc74 <bruno.hpn74@gmail.com>
2022-08-14 16:03:21 -0300

    dependência cors adicionada

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+const cors = require('cors');
 const express = require('express');
 const router = require('./router');
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 router(app);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`servidor rodando na porta ${port}`));
-
-module.exports = app;
