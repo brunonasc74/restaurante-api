fbcf673f564a554e1f14c5a9f49d22318c8578e5
brunonasc74 <bruno.hpn74@gmail.com>
2022-08-14 15:44:51 -0300

    mesa controller e route refatorados

--- src/router/mesasRoute.js
+++ src/router/mesasRoute.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const MesaController = require('../controllers/MesaController');
 
-
 const router = Router();
 
-router.get('/mesas', MesaController.pegaTodasMesas);
-router.get('/mesas/:id', MesaController.pegaUmaMesa);
-router.post('/mesas', MesaController.criaReserva);
-router.put('/mesas/:id', MesaController.atualizaReserva);
-router.delete('/mesas/:id', MesaController.apagaReserva);
+router
+	.get('/mesas', MesaController.pegaTodasMesas)
+	.get('/mesas/:id', MesaController.pegaUmaMesa)
+	.post('/mesas', MesaController.criaMesa)
+	.put('/mesas/:id', MesaController.atualizaMesa)
+	.delete('/mesas/:id', MesaController.apagaMesa);
 
 module.exports = router;
